8aaa3c16155a03244f826e455fac2022ab2eff92
victorfco845 <victorfco845@gmail.com>
2024-03-16 18:53:03 -0700

    se termino el proyecto between pages

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/biblioteca/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "biblioteca";
+const charset = "charset=utf8";
+?>
